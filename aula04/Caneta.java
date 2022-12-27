14203c3fac9d0aedea66c7305749534b2c9e06aa
HamiltonStefani <88253849+HamiltonStefani@users.noreply.github.com>
2022-12-27 16:22:57 -0300

    Add files via upload

---
+++ aula04/Caneta.java
@@ -0,0 +1,46 @@
+
+package aula04;
+public class Caneta {
+    public String modelo;
+    private float ponta;
+    private boolean tampada;
+    private  String cor; 
+    
+    public Caneta(){ //  este é o método costrutor, tem o mesmo nome da classe.
+        this.tampar();
+        this.cor = "Azul";
+    }
+    
+    public String getModelo(){
+        return this.modelo;
+    }
+    
+    public void setModelo(String m){
+        this.modelo = m;
+    }
+    
+    public float getPonta(){
+        return this.ponta;
+    }
+    
+    public void setPonta(float p){
+        this.ponta  =p;
+    }
+    
+    public void tampar(){
+        this.tampada = true;
+    }
+    
+    public void destampar(){
+        this.tampada = false;
+    }
+    
+    public void status(){
+        System.out.println("SOBRE A CANETA");
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Tampada: " + this.tampada);
+}
+   
+}
